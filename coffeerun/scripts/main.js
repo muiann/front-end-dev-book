8f410ca414f03533fff5f94f9c8b3abf1fa84955
unknown <muiann>
2022-10-07 13:28:15 +0500

    com

--- coffeerun/scripts/main.js
+++ coffeerun/scripts/main.js
@@ -1,8 +1,14 @@
 (function (window) {
   'use strict';
+  var FORM_SELECTOR = '[data-coffee-order="form"]';
   var App = window.App;
   var Truck = App.Truck;
-  var DataStore;
-  var myTruck = new Truck ('ncc-1701',new DataStore());
+  var DataStore = App.DataStore;
+  var FormHandler = App.FormHandler;
+  var myTruck = new Truck ('ncc-1701', new DataStore());
   window.myTruck = myTruck;
-})(window);
+  var formHandler = new FormHandler(FORM_SELECTOR);
+
+  formHandler.addSubmitHandler(myTruck.createOrder.bind(myTruck));
+  console.log(formHandler);
+}) (window);
